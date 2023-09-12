fe1f395466f7f6ee8ee619cf834e914f8b6d109e
Rodrigo Hernández Vanegas <64353819+rodhervan@users.noreply.github.com>
2023-09-12 02:07:10 -0500

    Add files via upload

--- dense_optical_flow.py
+++ dense_optical_flow.py
@@ -12,7 +12,21 @@ while(1):
         break
 
     next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
-    flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    prev = prvs
+    next
+    flow=None
+    pyr_scale = 0.5
+    levels=3
+    winsize=15
+    iterations=3
+    poly_n=5
+    poly_sigma=1.2
+    flags=0
+    # flow = cv.calcOpticalFlowFarneback(	prev = prvs, next, flow=None, pyr_scale = 0.5, levels=3, winsize=15,
+    #                                    iterations=3, poly_n=5, poly_sigma=1.2, flags=0)
+    
+    # flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    flow = cv.calcOpticalFlowFarneback(	prev, next, flow, pyr_scale, levels, winsize, iterations, poly_n, poly_sigma, flags)
     mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
     hsv[..., 0] = ang*180/np.pi/2
     hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
