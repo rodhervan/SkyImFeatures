6aaab17f5c672c2a905cd8dffd3678dd6f05bb58
Rodrigo Hernández Vanegas <64353819+rodhervan@users.noreply.github.com>
2023-12-03 20:25:02 -0500

    Add files via upload

--- two_frames_OF.py
+++ two_frames_OF.py
@@ -11,12 +11,16 @@ from skimage import measure
 from skimage.measure import label,regionprops
 from skimage.morphology import closing, square
 from skimage.color import label2rgb
+from skimage import io, color, segmentation
 from scipy import ndimage as ndi
 from scipy.ndimage import measurements, center_of_mass, binary_dilation, zoom
 import blend_modes
 import plotly.graph_objects as go
+import pandas as pd
 
-# 5, 6, 7, 8 y 22
+from skimage.draw import line, line_aa
+
+
 
 # angulo de elevacion
 
@@ -29,6 +33,62 @@ import plotly.graph_objects as go
 # filepath = '20230807_normal/20230807175600.png'
 # filepath = '20230807_normal/20230807210600.png'
 # filepath = '20230807_normal/20230807211330.png'
+
+def TwoDToRGBA (img):
+    background_img_raw = img
+    background_img = np.array(background_img_raw)  # Inputs to blend_modes need to be numpy arrays.
+    # Rescale the pixel values to the range [0, 255]
+    scaled_img = ((background_img - np.min(background_img)) / (np.max(background_img) - np.min(background_img)) * 255).astype(np.uint8)
+    # Create an RGBA image with the rescaled content
+    rgba_image = np.zeros((background_img.shape[0], background_img.shape[1], 4), dtype=np.uint8)
+    # Copy the rescaled values to the first three channels (RGB)
+    rgba_image[:, :, 0:3] = scaled_img[:, :, np.newaxis]
+    # Set the alpha channel to a constant value (255 for fully opaque)
+    alpha_value = 255
+    rgba_image[:, :, 3] = alpha_value
+    # Now 'rgba_image' is an RGBA image with dimensions (480, 640, 4)
+    return rgba_image
+
+def TwoDToRGB(img):
+    background_img_raw = img
+    background_img = np.array(background_img_raw)  # Inputs to blend_modes need to be numpy arrays.
+    # Rescale the pixel values to the range [0, 255]
+    scaled_img = ((background_img - np.min(background_img)) / (np.max(background_img) - np.min(background_img)) * 255).astype(np.uint8)
+    # Create an RGB image with the rescaled content
+    rgb_image = np.zeros((background_img.shape[0], background_img.shape[1], 3), dtype=np.uint8)
+    # Copy the rescaled values to all three channels (RGB)
+    rgb_image[:, :, :] = scaled_img[:, :, np.newaxis]
+    # Now 'rgb_image' is an RGB image with dimensions (height, width, 3)
+    return rgb_image
+
+def create_radial_gradient(size, center, radius):
+
+    y, x = np.ogrid[:size[0], :size[1]]
+    distance = np.sqrt((x - center[1]) ** 2 + (y - center[0]) ** 2)
+    gradient = 1 - np.clip(distance / radius, 0, 1)
+    return gradient
+
+def multiply_with_gradient(image, gradient, opacity):
+    # Inputs to blend_modes need to be floats.
+    background_img_float = image.astype(float)
+    foreground_img_float = gradient.astype(float)
+    blended_img_float = blend_modes.multiply(background_img_float, foreground_img_float, opacity)
+
+    # Convert blended image back into PIL image
+    blended_img = np.uint8(blended_img_float)  # Image needs to be converted back to uint8 type for PIL handling.
+    # blended_img_raw = Image.fromarray(blended_img)  # Note that alpha channels are displayed in black by PIL by default.
+    return blended_img
+
+def remove_small(slc, c=0.0001):
+    new_slc = slc.copy()
+    labels = label(slc,connectivity=1,background=0)
+    rps = regionprops(labels)
+    areas = np.array([r.area for r in rps])
+    idxs = np.where(areas/(640*480) < c)[0]
+    for i in idxs:
+        new_slc[tuple(rps[i].coords.T)] = 0
+    return new_slc
+
 def segmentation (filepath):
     img = Image.open(filepath)
     img = asarray(img)
@@ -39,45 +99,10 @@ def segmentation (filepath):
     slicee = camara[:,:,3]
     img = img* slicee
     
-    
-    def TwoDToRGBA (img):
-        background_img_raw = img
-        background_img = np.array(background_img_raw)  # Inputs to blend_modes need to be numpy arrays.
-        # Rescale the pixel values to the range [0, 255]
-        scaled_img = ((background_img - np.min(background_img)) / (np.max(background_img) - np.min(background_img)) * 255).astype(np.uint8)
-        # Create an RGBA image with the rescaled content
-        rgba_image = np.zeros((background_img.shape[0], background_img.shape[1], 4), dtype=np.uint8)
-        # Copy the rescaled values to the first three channels (RGB)
-        rgba_image[:, :, 0:3] = scaled_img[:, :, np.newaxis]
-        # Set the alpha channel to a constant value (255 for fully opaque)
-        alpha_value = 255
-        rgba_image[:, :, 3] = alpha_value
-        # Now 'rgba_image' is an RGBA image with dimensions (480, 640, 4)
-        return rgba_image
-    
     rgba_image = TwoDToRGBA (img)
-    
-    def TwoDToRGB(img):
-        background_img_raw = img
-        background_img = np.array(background_img_raw)  # Inputs to blend_modes need to be numpy arrays.
-        # Rescale the pixel values to the range [0, 255]
-        scaled_img = ((background_img - np.min(background_img)) / (np.max(background_img) - np.min(background_img)) * 255).astype(np.uint8)
-        # Create an RGB image with the rescaled content
-        rgb_image = np.zeros((background_img.shape[0], background_img.shape[1], 3), dtype=np.uint8)
-        # Copy the rescaled values to all three channels (RGB)
-        rgb_image[:, :, :] = scaled_img[:, :, np.newaxis]
-        # Now 'rgb_image' is an RGB image with dimensions (height, width, 3)
-        return rgb_image
-    
+
     # rgba_image = TwoDToRBGA (img)
-    
-    def create_radial_gradient(size, center, radius):
-    
-        y, x = np.ogrid[:size[0], :size[1]]
-        distance = np.sqrt((x - center[1]) ** 2 + (y - center[0]) ** 2)
-        gradient = 1 - np.clip(distance / radius, 0, 1)
-        return gradient
-    
+
     image_size = img.shape
     gradient_center = (235, 314)  # Center of the gradient
     gradient_radius = 200  # Radius of the gradient
@@ -88,16 +113,6 @@ def segmentation (filepath):
     gradient_2 = create_radial_gradient(image_size, gradient_center, gradient_radius)
     rgba_grad_2 = TwoDToRGBA (gradient_2)
     
-    def multiply_with_gradient(image, gradient, opacity):
-        # Inputs to blend_modes need to be floats.
-        background_img_float = image.astype(float)
-        foreground_img_float = gradient.astype(float)
-        blended_img_float = blend_modes.multiply(background_img_float, foreground_img_float, opacity)
-    
-        # Convert blended image back into PIL image
-        blended_img = np.uint8(blended_img_float)  # Image needs to be converted back to uint8 type for PIL handling.
-        blended_img_raw = Image.fromarray(blended_img)  # Note that alpha channels are displayed in black by PIL by default.
-        return blended_img
     
     first_grad = multiply_with_gradient(rgba_image, rgba_grad_1, 1)
     second_grad = multiply_with_gradient(first_grad, rgba_grad_2, 0)
@@ -113,16 +128,6 @@ def segmentation (filepath):
     mask.shape
     mask = clear_border(mask)
     
-    
-    def remove_small(slc, c=0.0001):
-        new_slc = slc.copy()
-        labels = label(slc,connectivity=1,background=0)
-        rps = regionprops(labels)
-        areas = np.array([r.area for r in rps])
-        idxs = np.where(areas/(640*480) < c)[0]
-        for i in idxs:
-            new_slc[tuple(rps[i].coords.T)] = 0
-        return new_slc
     
     big_mask = remove_small(mask)
     mask_labeled = label(big_mask)
@@ -157,11 +162,7 @@ plt.imshow(mask)
 # plt.pcolormesh(mask)
 
 
-
-
-
 import time
-import pandas as pd
 # import os
 
 
@@ -223,33 +224,64 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-
-
-from skimage import io, color, segmentation
-
-
+#### Label for velocity 
 # Read the image
 image = draw_hsv_img
-
 # Convert the image to the Lab color space
 image_lab = color.rgb2lab(image)
-
 # Separate the L, a, and b channels
 L, a, b = image_lab[:, :, 0], image_lab[:, :, 1], image_lab[:, :, 2]
-
 # Use Otsu's thresholding on the L channel to create a binary mask
 thresh = threshold_otsu(L)
 binary_mask = L > thresh
-
+small_binary = remove_small(binary_mask)
 # Label connected components in the binary mask
-labeled_image, num_labels = label(binary_mask, connectivity=2, return_num=True)
-
+labeled_image, num_labels = label(small_binary, connectivity=2, return_num=True)
 # Display the original image, binary mask, and labeled image
 io.imshow(image)
 io.show()
 
-
 io.imshow(labeled_image)
 io.show()
+
+
+
+for num in range(1,num_labels+1):
+    mask = labeled_image == num
+    x_flow = flow[:,:,0] * mask
+    y_flow = flow[:,:,1] * mask
+    non_zero_elements_x = x_flow[x_flow != 0]
+    non_zero_elements_y = y_flow[y_flow != 0]
+    average_non_zero_x = np.average(non_zero_elements_x)
+    average_non_zero_y = np.average(non_zero_elements_y)
+    
+    over_img = img[:,:,0] * mask
+    mask_over_img = over_img !=0
+    small_ovr = remove_small(mask_over_img, c=0.0001)
+    label_over_small, small_id = label(small_ovr, return_num=True)
+
+    for labl in range(1,small_id+1): 
+        centroide = measure.centroid(label_over_small == labl)   
+        line_img = cv2.arrowedLine(img,
+                            (int(centroide[1]), int(centroide[0])),
+                            (int(centroide[1] + average_non_zero_x * 5), int(centroide[0] + average_non_zero_y * 5)),
+                            (0,255,0), 2)
+        
+        
+io.imshow(line_img)
+    
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
 
 
