b6731ed4229b1d40cbd14f5f34f53193bbe83c4b
Rodrigo Hernández Vanegas <64353819+rodhervan@users.noreply.github.com>
2023-12-05 05:07:01 -0500

    Add files via upload

--- bulk_all.py
+++ bulk_all.py
@@ -99,11 +99,9 @@ def get_time (filepath):
     ss = st[4:6]
     timer = hh + ':'+ mm + ':' + ss
     
-    
-    
     return timer
 
-def get_solar_coords (x_mapped, y_mapped, day, timer):
+def get_solar_coords (x_mapped, y_mapped, day, timer):   
     x = x_mapped[day + ' ' + timer + '-05:00']
     y = y_mapped[day + ' ' + timer + '-05:00']
     return x, y
@@ -251,146 +249,181 @@ def draw_hsv(flow):
     return bgr, mag, ang, hsv
 
 
-# filepath = '20230807_normal/20230807152030.png'
-# filepath = '20230807_normal/20230807152100.png'
-
-
-# filepath = '20230807_normal/20230807152030.png'
-# next_filepath = '20230807_normal/20230807152100.png'
-
-# filepath = '20230807_normal/20230807171730.png'
-# next_filepath = '20230807_normal/20230807171800.png'
-
-filepath = '20230807_normal/20230807180130.png'
-next_filepath = '20230807_normal/20230807180200.png'
 
 
 
-prev_mask, prev_img = segmentation(filepath)
-mask, img = segmentation(next_filepath)
+
+# Create the 'modified' directory if it doesn't exist
+output_directory = '20230807_sun_pos'
+os.makedirs(output_directory, exist_ok=True)
 
 
-# plt.imshow(prev_img)
-# plt.imshow(mask)
+# iterate over files in
+# that directory
+directory = '20230807_normal'
+first_image = '20230807000000.png'
 
-# plt.pcolormesh(mask)
+filepath = directory + '/' + first_image
 
-
-
-prevgray = cv2.cvtColor(prev_img, cv2.COLOR_BGR2GRAY)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# start time to calculate FPS
-start = time.time()
-
-flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-# prevgray = gray
-
-# End time
-end = time.time()
-# calculate the FPS for current frame detection
-fps = 1 / (end-start)
-# print(f"{fps:.2f} FPS")
-
-draw_flow_img = draw_flow(gray, flow)
-draw_flow_prev = draw_flow(prevgray, flow)
-draw_hsv_img, mag, ang, hsv = draw_hsv(flow)
-# cv2.imshow('flow', draw_flow_img)
-# cv2.imshow('flow_prev', draw_flow_prev)
-# cv2.imshow('flow HSV', draw_hsv_img)
-key = cv2.waitKey(5)
-
-# cv2.imwrite('flow.png', draw_flow_img)
-# cv2.imwrite('hsv.png', draw_hsv_img)
-
-# cv2.waitKey(0)
-cv2.destroyAllWindows()
+# directory = 'test_folder'
+# directory = 'nf'
+for filename in os.scandir(directory):
+    if filename.is_file():
+        name = str(filename).replace('<DirEntry ','').replace('>','').replace("'","")
+        next_filepath = directory + '/' + name 
 
 
-#### Label for velocity 
-# Read the image
-image = draw_hsv_img
-# Convert the image to the Lab color space
-image_lab = color.rgb2lab(image)
-# Separate the L, a, and b channels
-L, a, b = image_lab[:, :, 0], image_lab[:, :, 1], image_lab[:, :, 2]
-# Use Otsu's thresholding on the L channel to create a binary mask
-thresh = threshold_otsu(L)
-binary_mask = L > thresh
-small_binary = remove_small(binary_mask)
-# Label connected components in the binary mask
-labeled_image, num_labels = label(small_binary, connectivity=2, return_num=True)
-# Display the original image, binary mask, and labeled image
-# io.imshow(image)
-# io.show()
-
-# io.imshow(labeled_image)
-# io.show()
-
-
-for num in range(1,num_labels+1):
-    num_mask = labeled_image == num
-    x_flow = flow[:,:,0] * num_mask
-    y_flow = flow[:,:,1] * num_mask
-    non_zero_elements_x = x_flow[x_flow != 0]
-    non_zero_elements_y = y_flow[y_flow != 0]
-    average_non_zero_x = np.average(non_zero_elements_x)
-    average_non_zero_y = np.average(non_zero_elements_y)
-    
-    over_img = img[:,:,0] * num_mask
-    mask_over_img = over_img !=0
-    small_ovr = remove_small(mask_over_img, c=0.0001)
-    label_over_small, small_id = label(small_ovr, return_num=True)
-
-    for labl in range(1,small_id+1): 
-        centroide = measure.centroid(label_over_small == labl)   
-        line_img = cv2.arrowedLine(img,
-                            (int(centroide[1]), int(centroide[0])),
-                            (int(centroide[1] + average_non_zero_x * 5), int(centroide[0] + average_non_zero_y * 5)),
-                            (0,255,0), 2)
+        # filepath = '20230807_normal/20230807152030.png'
+        # filepath = '20230807_normal/20230807152100.png'
         
         
-# io.imshow(line_img)
+        # filepath = '20230807_normal/20230807152030.png'
+        # next_filepath = '20230807_normal/20230807152100.png'
+        
+        # filepath = '20230807_normal/20230807171730.png'
+        # next_filepath = '20230807_normal/20230807171800.png'
+        
+        # filepath = '20230807_normal/20230807180130.png'
+        # next_filepath = '20230807_normal/20230807180200.png'
+        
+        
+        
+        prev_mask, prev_img = segmentation(filepath)
+        mask, img = segmentation(next_filepath)
+        
+        
+        # plt.imshow(prev_img)
+        # plt.imshow(mask)
+        
+        # plt.pcolormesh(mask)
+        
+        
+        
+        prevgray = cv2.cvtColor(prev_img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        # start time to calculate FPS
+        start = time.time()
+        
+        flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        
+        filepath = next_filepath
+        # prevgray = gray
+        
+        # End time
+        end = time.time()
+        # calculate the FPS for current frame detection
+        fps = 1 / (end-start)
+        # print(f"{fps:.2f} FPS")
+        
+        draw_flow_img = draw_flow(gray, flow)
+        draw_flow_prev = draw_flow(prevgray, flow)
+        draw_hsv_img, mag, ang, hsv = draw_hsv(flow)
+        # cv2.imshow('flow', draw_flow_img)
+        # cv2.imshow('flow_prev', draw_flow_prev)
+        # cv2.imshow('flow HSV', draw_hsv_img)
+        key = cv2.waitKey(5)
+        
+        # cv2.imwrite('flow.png', draw_flow_img)
+        # cv2.imwrite('hsv.png', draw_hsv_img)
+        
+        # cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        
+        #### Label for velocity 
+        # Read the image
+        image = draw_hsv_img
+        # Convert the image to the Lab color space
+        image_lab = color.rgb2lab(image)
+        # Separate the L, a, and b channels
+        L, a, b = image_lab[:, :, 0], image_lab[:, :, 1], image_lab[:, :, 2]
+        # Use Otsu's thresholding on the L channel to create a binary mask
+        thresh = threshold_otsu(L)
+        binary_mask = L > thresh
+        small_binary = remove_small(binary_mask)
+        # Label connected components in the binary mask
+        labeled_image, num_labels = label(small_binary, connectivity=2, return_num=True)
+        # Display the original image, binary mask, and labeled image
+        # io.imshow(image)
+        # io.show()
+        
+        # io.imshow(labeled_image)
+        # io.show()
+        
+        line_img = img
+        for num in range(1,num_labels+1):
+            num_mask = labeled_image == num
+            x_flow = flow[:,:,0] * num_mask
+            y_flow = flow[:,:,1] * num_mask
+            non_zero_elements_x = x_flow[x_flow != 0]
+            non_zero_elements_y = y_flow[y_flow != 0]
+            average_non_zero_x = np.average(non_zero_elements_x)
+            average_non_zero_y = np.average(non_zero_elements_y)
+            
+            over_img = img[:,:,0] * num_mask
+            mask_over_img = over_img !=0
+            small_ovr = remove_small(mask_over_img, c=0.0001)
+            label_over_small, small_id = label(small_ovr, return_num=True)
+        
+            for labl in range(1,small_id+1): 
+                centroide = measure.centroid(label_over_small == labl)   
+                line_img = cv2.arrowedLine(img,
+                                    (int(centroide[1]), int(centroide[0])),
+                                    (int(centroide[1] + average_non_zero_x * 5), int(centroide[0] + average_non_zero_y * 5)),
+                                    (0,255,0), 2)
+                
+                
+        # io.imshow(line_img)
+        
+        
+        
+        # solpos.apparent_zenith['2023-08-07 18:01:30-05:00']
+        
+        
+        # filepath = '20230808_normal/20230808190630.png'
+            
+        x_mapped, y_mapped, day = solar_pos(filepath)
+        timer = get_time (filepath)
+        if int(timer[:-6]) <5:
+            timer = '05:47:00'
+            solar_x, solar_y =  get_solar_coords (x_mapped, y_mapped, day, timer)
+            covered = 'No sun'
+        if (int(timer[:-6]) ==5)&(int(timer[-5:-3])<47):
+            timer = '05:47:00'
+            solar_x, solar_y =  get_solar_coords (x_mapped, y_mapped, day, timer)
+            covered = 'No sun'
+        else:    
+            solar_x, solar_y =  get_solar_coords (x_mapped, y_mapped, day, timer)
+            covered = mask[round(solar_x),round(solar_y)] > 0
 
-
-# solpos.apparent_zenith['2023-08-07 18:01:30-05:00']
-
-
-# filepath = '20230808_normal/20230808190630.png'
-    
-x_mapped, y_mapped, day = solar_pos(filepath)
-timer = get_time (filepath)
-solar_x, solar_y =  get_solar_coords (x_mapped, y_mapped, day, timer)
-
-
-covered = mask[round(solar_x),round(solar_y)] > 0
-
-
-# img = Image.open(filepath)
-# img = asarray(img)
-# raw = img
-# camara = Image.open('camara.png')
-# camara = asarray(camara)
-# slicee = camara[:,:,3]
-# img = img* slicee
-# from_array = Image.fromarray(img)
-# im_flip = ImageOps.flip(from_array)
-# im_rotate = im_flip.rotate(-3.3, center=(314, 235))
-# img = asarray(im_rotate)
-
-img = line_img
-# Create a figure and axis for the imag
-fig, ax = plt.subplots()
-ax.imshow(img, extent=[0, img.shape[1], 0, img.shape[0]])
-ax.plot(solar_x, solar_y, 'r.', markersize=3, label=label)
-ax.text(0.2, 0.9, 'sun covered = '+str(covered), color = 'r',horizontalalignment='center',
-     verticalalignment='center', transform=ax.transAxes)
-# ax.plot(x_mapped, y_mapped,'r' , label=label)
-
-plt.savefig('foo.png')
-plt.close() 
-# plt.show()
-
-
+        
+        
+        # img = Image.open(filepath)
+        # img = asarray(img)
+        # raw = img
+        # camara = Image.open('camara.png')
+        # camara = asarray(camara)
+        # slicee = camara[:,:,3]
+        # img = img* slicee
+        # from_array = Image.fromarray(img)
+        # im_flip = ImageOps.flip(from_array)
+        # im_rotate = im_flip.rotate(-3.3, center=(314, 235))
+        # img = asarray(im_rotate)
+        
+        img = line_img
+        # Create a figure and axis for the imag
+        fig, ax = plt.subplots()
+        ax.imshow(img, extent=[0, img.shape[1], 0, img.shape[0]])
+        ax.plot(solar_x, solar_y, 'r.', markersize=3, label=label)
+        ax.text(0.2, 0.9, 'sun covered = '+str(covered), color = 'r',horizontalalignment='center',
+             verticalalignment='center', transform=ax.transAxes)
+        # ax.plot(x_mapped, y_mapped,'r' , label=label)
+        new_path = os.path.join(output_directory, name)
+        plt.savefig(new_path)
+        plt.close() 
+        # plt.show()
+        
+        
 
 
